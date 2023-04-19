6bd2e2a60d6589976e4988b8cbd2564261ae27ed
GrimNinjaFox <128336362+GrimNinjaFox@users.noreply.github.com>
2023-04-18 20:49:45 -0500

    Create Students.java

---
+++ Students.java
@@ -0,0 +1,15 @@
+package com.example.Project2;
+
+public class Students {
+
+	public Students(String name) {
+		// TODO Auto-generated constructor stub
+	}
+	String id[] = {"1,2,3,4"};
+	String firstName[] = {"Caleb,Aida,Alex,Meba"};
+	String gpa[] = {"3.6","3.1","3.4","3.2"};
+	String email[] = {"jdtg0@aws,com","adattt@test.gov","Adavis@bccs.com","mtravis@yahoo.com"};
+	String gender[] = {"Male","Female"};
+	
+
+}
